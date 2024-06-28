22a49e27600d213fce2396fbf811c4fdbea09fb2
DreamyZero <BoGuUs2@mail.ru>
2024-06-28 11:20:11 +0300

    second commit

--- components/Media.js
+++ components/Media.js
@@ -6,17 +6,13 @@ import { gStyles } from '../styles/style.js';
 
 
 export default function App() {
-    // (Музыка) Создание состояния sound (объект звука) с помощью хука useState(). Переменная setSound - функция для обновления
     const [sound, setSound] = useState();
     const [isMusicPlaying, setIsMusicPlaying] = useState(false);
 
-    // (Музыка) Ассинхронная функция операций с музыкой
     async function playSound() {
         if(!isMusicPlaying) {
-            // Получаем файл музыки
             const { sound } = await Audio.Sound.createAsync(require('../assets/Music.mp3'));
             setSound(sound);
-            // Включить музыку
             await sound.playAsync();
             setIsMusicPlaying(true);
         } else {
